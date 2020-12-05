bfd5b348aaf54c51381c3b3c3d0d26c6b9568970
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-12-05 22:35:32 +0100

    void unused parameters from main

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,9 @@ void usage(FILE *stream) {
 
 #include <unistd.h>
 int main(int argc, char *argv[], char *envp[]) {
+    (void)argc;
+    (void)argv;
+    (void)envp;
     aids::console_width = 153;
 
     const int cnt{30};
