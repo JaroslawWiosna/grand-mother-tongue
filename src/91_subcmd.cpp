d88dd9956cc802cc60171bb32980ed466519bbe5
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-12-10 21:59:11 +0100

    (#48) Placeholder for subcmd search handling function

---
+++ src/91_subcmd.cpp
@@ -0,0 +1,9 @@
+#include "41_subcmd.hpp"
+
+void subcmd_search(const char *input) {
+    aids::println(stdout, "This is search handler (see #48)");
+
+    aids::println(stdout, "Input is: ", input);
+
+    exit(0);
+}
