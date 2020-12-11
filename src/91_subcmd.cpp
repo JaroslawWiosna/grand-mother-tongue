8950154db6636584b656a3839076b5e53440fec7
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-12-11 21:54:07 +0100

    (#48) Extract search's json (but fails on assert: obj `length == 1`)

--- src/91_subcmd.cpp
+++ src/91_subcmd.cpp
@@ -3,7 +3,15 @@
 void subcmd_search(const char *input) {
     aids::println(stdout, "This is search handler (see #48)");
 
-    aids::println(stdout, "Input is: ", input);
+    RestApiUrlRequests reqs{};
+    std::string url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=";
+    url += input;
+    url += "&language=en&format=json";
+    reqs.emplace_back(RestApiUrlRequest{url});
+
+    auto resps = request(reqs);
+    auto result = extract_1st_result_desc(resps[0].res.value());
+    println(stdout, result);
 
     exit(0);
 }
