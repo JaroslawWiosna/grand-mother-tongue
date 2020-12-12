7776fabd418e42f98b4aa86e5b748ad1281015ce
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-12-12 19:46:46 +0100

    (#48) Subcomand search that outputs Wikidata's ID
    Close #48

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- (#48) Subcomand search that outputs Wikidata ID
 
 ## [0.7.1] - 2020-12-07
 ### Fixed
--- src/91_subcmd.cpp
+++ src/91_subcmd.cpp
@@ -1,8 +1,8 @@
 #include "41_subcmd.hpp"
 
+// TODO: subcmd_search does not support `multi-words search`
+// TODO: subcmd_search prints only the first result
 void subcmd_search(const char *input) {
-    aids::println(stdout, "This is search handler (see #48)");
-
     RestApiUrlRequests reqs{};
     std::string url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=";
     url += input;
@@ -10,8 +10,65 @@ void subcmd_search(const char *input) {
     reqs.emplace_back(RestApiUrlRequest{url});
 
     auto resps = request(reqs);
-    auto result = extract_1st_result_desc(resps[0].res.value());
-    println(stdout, result);
+    // std::cout << resps[0].res.value() << '\n';
+
+    size_t size = 64 * 1024;
+    char *json = new char[size];
+    memset((void *)json, '\0', size);
+    memcpy(json, resps[0].res.value().c_str(), strlen(resps[0].res.value().c_str()));
+    defer(delete[] json);
+
+    struct json_value_s *root = json_parse(json, strlen(json));
+    assert(root != NULL);
+    defer(free(root));
+    struct json_object_s *object = json_value_as_object(root);
+    assert(object != NULL);
+
+    // const int result_cnt = object->length;
+    // std::cout << "result cnt == " << result_cnt << '\n';
+
+    struct json_object_s *wob = nullptr;
+    struct json_object_element_s *wobj = nullptr;
+
+    if (not wob) { 
+        wob = object;
+    };
+    wobj = wob->start;
+    while (wobj && 0 != strcmp(wobj->name->string, "search")) {
+        wobj = wobj->next;
+    }
+    assert(0 == strcmp(wobj->name->string, "search"));
+
+    struct json_array_s *arr_id = json_value_as_array(wobj->value);    
+    struct json_object_element_s *wobj_id = json_value_as_object(arr_id->start->value)->start;
+    // struct json_object_s *wob_id = json_value_as_object(wobj_id->value);
+
+    // std::cout << wobj_id->name->string << '\n';
+    char *buf = (char*)alloc(&region, 512);
+    memset(buf, 0, 512);
+    aids::String_Buffer sbuffer = {512, buf, {}};
+    aids::sprint(&sbuffer, json_value_as_string(wobj_id->value)->string);
+//    std::cout << buf << '\n';
+//    std::cout << '\n';
+
+    std::string id = buf;
+
+    while (wobj_id && 0 != strcmp(wobj_id->name->string, "description")) {
+        wobj_id = wobj_id->next;
+    }
+    assert(0 == strcmp(wobj_id->name->string, "description"));
+    // std::cout << json_value_as_string(wobj_id->value)->string << '\n';
+
+    char *buf2 = (char*)alloc(&region, 512);
+    memset(buf2, 0, 512);
+    sbuffer = {512, buf2, {}};
+    aids::sprint(&sbuffer, json_value_as_string(wobj_id->value)->string);
+    // std::cout << buf2 << '\n';
+    // std::cout << '\n';
+    std::string desc = buf2;
+
+    std::cout << "Result:\n";
+    std::cout << id << '\t' << desc << '\n';
 
     exit(0);
 }
